1f152e1e2d7567936b7ba5b67dd628726267f403
Catherine H <>
2021-04-01 22:38:45 +0000

    added turtles

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import turtle
+import random
 
 cameraMan = turtle.Turtle()
 cameraMan.shape("triangle")
 
 cameraMan.penup()
-cameraMan.goto(-200, 200)
+cameraMan.goto(-175, 140)
+cameraMan.speed(0)
 cameraMan.right(90)
 
 for i in range(16):
@@ -16,5 +18,51 @@ for i in range(16):
   cameraMan.right(180)
   cameraMan.forward(210)
   cameraMan.right(90)
-  cameraMan.forward(15)
+  cameraMan.forward(20)
   cameraMan.right(90)
+cameraMan.hideturtle()
+
+catherine = turtle.Turtle()
+catherine.shape("turtle")
+catherine.penup()
+catherine.goto(-200, 110 )
+catherine.color("#F2A8C6")
+
+coolKid = turtle.Turtle()
+coolKid.shape("turtle")
+coolKid.penup()
+coolKid.goto(-200, 70)
+coolKid.color("#E8C192")
+
+turtleJr = turtle.Turtle()
+turtleJr.shape("turtle")
+turtleJr.penup()
+turtleJr.goto(-200, 30)
+turtleJr.color("#92E8A3")
+
+yay = turtle.Turtle()
+yay.shape("turtle")
+yay.penup()
+yay.goto(-200, -10)
+yay.color("LightSlateBlue")
+
+coder = turtle.Turtle()
+coder.shape("turtle")
+coder.penup()
+coder.goto(-200, -50)
+coder.color("DarkOrchid")
+
+#Create 4 other turtles with different colors; set their locations
+
+catherine.pendown()
+coolKid.pendown()
+turtleJr.pendown()
+yay.pendown()
+coder.pendown()
+
+for i in range(40):
+  catherine.forward(random.randint(1,15))
+  coolKid.forward(random.randint(1,15))
+  turtleJr.forward(random.randint(1,15))
+  yay.forward(random.randint(1,15))
+  coder.forward(random.randint(1,15))
